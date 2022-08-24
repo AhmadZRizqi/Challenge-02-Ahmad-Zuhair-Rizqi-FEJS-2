15e0fadfd47fc43853e3bd578e2026e4781180f0
AhmadZRizqi <az.rizqi@gmail.com>
2022-08-24 15:39:57 +0700

    commit soal 2 finish

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ const kalimat2 = 'Gunung Bromo tak akan mampu menggambarkan besarnya cintaku pad
 
 console.log(changeWord('mencintai', 'membenci', kalimat1))
 console.log(changeWord('Bromo', 'Semeru', kalimat2))
+// No.1 END
+
+// No.2
+const checkTypeNumber = givenNumber => {
+    if (givenNumber%2 == 0 && typeof givenNumber == 'number' && typeof givenNumber !== 'string' && typeof givenNumber !== 'object') {
+        return "GENAP"
+    } else if(givenNumber%2 !== 0 && typeof givenNumber == 'number' && typeof givenNumber !== 'string' && typeof givenNumber !== 'object') {
+        return "GANJIL"
+    } else if(givenNumber == null){
+        return "Error: Bro Where is the parameter?"
+    } else{
+        return "Error: Invalid Data Type"
+    }
+}
+console.log(checkTypeNumber(10))
+console.log(checkTypeNumber(3))
+console.log(checkTypeNumber("3"))
+console.log(checkTypeNumber({}))
+console.log(checkTypeNumber([]))
+console.log(checkTypeNumber())
+// No.2 END
