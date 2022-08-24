9cfbe27acad1b1e76848aff575e52017448d556c
AhmadZRizqi <az.rizqi@gmail.com>
2022-08-24 14:56:14 +0700

    commit soal 1 finish

---
+++ index.js
@@ -0,0 +1,9 @@
+// No.1
+function changeWord (selectedText, changedText, text) {
+    return text.replace(selectedText, changedText);
+}
+const kalimat1 = 'Andini sangat mencintai kamu selamanya'
+const kalimat2 = 'Gunung Bromo tak akan mampu menggambarkan besarnya cintaku padamu'
+
+console.log(changeWord('mencintai', 'membenci', kalimat1))
+console.log(changeWord('Bromo', 'Semeru', kalimat2))
