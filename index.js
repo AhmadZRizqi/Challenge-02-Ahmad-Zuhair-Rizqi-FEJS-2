0a6a77bdefbcff82f7e9e15d6c48898c7677ea6e
AhmadZRizqi <az.rizqi@gmail.com>
2022-08-25 21:58:39 +0700

    soal nomer 6 sudah

--- index.js
+++ index.js
@@ -95,50 +95,71 @@
 // No.4 END --------------------------------------------------
 
 // No.5 ------------------------------------------------------
-function getSplitName(personName){
-    nama = {
-        firstname: '',
-        middlename: '',
-        lastname: '',
-    }
-    if (typeof personName == 'string'){
-        const splitName = personName.split(' ')
-        if (splitName.length == 2) {
-            namaDepan = splitName[0];
-            namaTengah = null;
-            namaBelakang = splitName[1];
-            nama.firstname = namaDepan;
-            nama.middlename = namaTengah;
-            nama.lastname = namaBelakang;
-            return nama
-        } else if (splitName.length == 1){
-            namaDepan = splitName[0];
-            namaTengah = null;
-            namaBelakang = null;
-            nama.firstname = namaDepan;
-            nama.middlename = namaTengah;
-            nama.lastname = namaBelakang;
-            return nama
-        } else if (splitName.length == 3) {
-            namaDepan = splitName[0];
-            namaTengah = splitName[1];
-            namaBelakang = splitName[2];
-            nama.firstname = namaDepan;
-            nama.middlename = namaTengah;
-            nama.lastname = namaBelakang;
-            return nama
-        } else if (splitName.length > 3){
-            return "Error: Nama lebih dari 3 suku kata"
-        }
+// function getSplitName(personName){
+//     nama = {
+//         firstname: '',
+//         middlename: '',
+//         lastname: '',
+//     }
+//     if (typeof personName == 'string'){
+//         const splitName = personName.split(' ')
+//         if (splitName.length == 2) {
+//             namaDepan = splitName[0];
+//             namaTengah = null;
+//             namaBelakang = splitName[1];
+//             nama.firstname = namaDepan;
+//             nama.middlename = namaTengah;
+//             nama.lastname = namaBelakang;
+//             return nama
+//         } else if (splitName.length == 1){
+//             namaDepan = splitName[0];
+//             namaTengah = null;
+//             namaBelakang = null;
+//             nama.firstname = namaDepan;
+//             nama.middlename = namaTengah;
+//             nama.lastname = namaBelakang;
+//             return nama
+//         } else if (splitName.length == 3) {
+//             namaDepan = splitName[0];
+//             namaTengah = splitName[1];
+//             namaBelakang = splitName[2];
+//             nama.firstname = namaDepan;
+//             nama.middlename = namaTengah;
+//             nama.lastname = namaBelakang;
+//             return nama
+//         } else if (splitName.length > 3){
+//             return "Error: Nama lebih dari 3 suku kata"
+//         }
 
-    } else {
-        return "Error: Tipe data tidak boleh selain string"
-    }
+//     } else {
+//         return "Error: Tipe data tidak boleh selain string"
+//     }
 
-}
-console.log(getSplitName('Aldi Daniela Pranata'))
-console.log(getSplitName('Dwi Kuncoro'))
-console.log(getSplitName('Aurora'))
-console.log(getSplitName('Aurora Aurelia Sukma Darma'))
-console.log(getSplitName(0))
+// }
+// console.log(getSplitName('Aldi Daniela Pranata'))
+// console.log(getSplitName('Dwi Kuncoro'))
+// console.log(getSplitName('Aurora'))
+// console.log(getSplitName('Aurora Aurelia Sukma Darma'))
+// console.log(getSplitName(0))
 // No.5 END --------------------------------------------------
+
+// No.6 ------------------------------------------------------
+function getAngkaTerbesarKedua(dataAngka) {
+    if (typeof dataAngka == 'object') {
+        dataAngka.sort(function(a,b){
+            return b-a
+        })
+        terbesarKedua = dataAngka[1];
+        return terbesarKedua
+    } else if(dataAngka == null) {
+        return "Error: Data tidak boleh kosong"
+    } else {
+        return "Error: Tipe data salah, harus berupa object"
+    }
+}
+    
+const dataAngka = [9,4,7,7,4,3,2,2,8]
+console.log(getAngkaTerbesarKedua(dataAngka))
+console.log(getAngkaTerbesarKedua(0))
+console.log(getAngkaTerbesarKedua())
+// No.6 END --------------------------------------------------
